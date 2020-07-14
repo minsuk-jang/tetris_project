53b6bce853a8046c851275a23a17a7ef1e62b7bf
jms8732 <jms8732@dongguk.edu>
2020-07-14 21:40:28 +0900

    2020-7-14. 테트리스 구성
    1) 1초 후에 떨어지는 동작을 Timer를 이용하여 나눠서 구현

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/4c/60cd3a2ecbc5001a183d84bb87fd0812
@@ -0,0 +1,8 @@
+package toy_project;
+
+public class play_game {
+	public static void main(String[] args) {
+		PlayThread pt =new PlayThread();
+		pt.start();
+	}
+}
--- toy_project/src/toy_project/play_game.java
+++ toy_project/src/toy_project/play_game.java
@@ -1,8 +1,19 @@
 package toy_project;
+import java.util.*;
 
 public class play_game {
 	public static void main(String[] args) {
 		PlayThread pt =new PlayThread();
+		Timer timer = new Timer();
+		TimerTask task = new TimerTask() {
+			@Override 
+			public void run() {
+				PlayThread.dir = 0;
+			}
+		};
+		
+		timer.schedule(task, 0,1000);
+		
 		pt.start();
 	}
 }
